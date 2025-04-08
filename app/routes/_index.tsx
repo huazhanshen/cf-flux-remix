323175e7b8e2fb6b13e8834b2e0c0e324b3ed0ac
huazhanshen <39611455+huazhanshen@users.noreply.github.com>
2025-04-08 13:02:08 +0800

    Update _index.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -41,7 +41,7 @@ export default function Index() {
                 to="/generate-image"
                 className="block w-full text-center px-6 py-3 text-lg font-semibold text-white bg-gradient-to-r from-indigo-500 to-indigo-700 rounded-xl transition transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-indigo-400"
               >
-                白嫖 CF 的 Flux 生成图片
+                Flux 图片生成
               </Link>
             </li>
             <li>
@@ -49,15 +49,7 @@ export default function Index() {
                 to="https://github.com/aigem/cf-flux-remix"
                 className="block w-full text-center px-6 py-3 text-lg font-semibold text-white bg-gradient-to-r from-indigo-500 to-indigo-700 rounded-xl transition transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-indigo-400"
               >
-                访问 Github
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="https://github.com/aigem/CFr2-webdav"
-                className="block w-full text-center px-6 py-3 text-lg font-semibold text-white bg-gradient-to-r from-indigo-500 to-indigo-700 rounded-xl transition transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-indigo-400"
-              >
-                Workers+R2搭建个人免费webdav网盘
+                作者 Github
               </Link>
             </li>
             <li>
